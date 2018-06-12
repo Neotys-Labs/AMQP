b5bad39d180b310172c460aa8303e7f6b3fdb4b6
Sébastien Richert <sebastien.richert@neotys.com>
2018-06-12 10:04:39 +0200

    Support of 6.5.1 as minimum version.

--- src/main/java/com/neotys/amqp/common/AMQPAction.java
+++ src/main/java/com/neotys/amqp/common/AMQPAction.java
@@ -15,7 +15,7 @@ public abstract class AMQPAction implements Action{
 	private static final String BUNDLE_NAME = "com.neotys.amqp.common.bundle";
 	private static final String DISPLAY_PATH = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault()).getString("amqp.displayPath");
 	
-	private static final Optional<String> MINIMUM_NEOLOAD_VERSION = Optional.of("6.6");
+	private static final Optional<String> MINIMUM_NEOLOAD_VERSION = Optional.of("6.5.1");
 	private static final Optional<String> MAXIMUM_NEOLOAD_VERSION = Optional.absent();
 	
 	@Override
